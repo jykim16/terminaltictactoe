3d3e442b0971035644303c6dc99e6d1212373ca7
Jonathan Kim <jykim16@gmail.com>
2017-09-25 18:16:23 -0700

    completed tictactoe

--- tictactoe.js
+++ tictactoe.js
@@ -1,28 +1,64 @@
 var board = [[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']];
 var turn = 'X';
-
+var victor;
 console.log(`
   Welcome to TicTacToe
   press enter to begin.
   1-9 for placing a piece.
   `)
+console.log('Player 1 input your name!')
 
 let checkVictory = () => {
-  //if board is in some configuration then return true;
-  return false;
+  var result = false;
+  var col1 = '';
+  var col2 = '';
+  var col3 = '';
+  var diagMajor = '';
+  var diagMinor = '';
+  var checkAxis = (axis) => {
+    if(axis === 'XXX' || axis === 'OOO') {
+      result = true;
+    }
+  }
+  board.forEach((row, i)=>{
+    var rowJoin = row.join('');
+    checkAxis(rowJoin);
+    col1+=row[0];
+    col2+=row[1];
+    col3+=row[2];
+    diagMajor += row[i];
+    diagMinor += row[2-i];
+  })
+  checkAxis(col1);
+  checkAxis(col2);
+  checkAxis(col3);
+  checkAxis(diagMajor)
+  checkAxis(diagMinor)
+
+  return result;
 }
+
 var stdin = process.openStdin();
 
 stdin.addListener("data", function(d) {
     var input = +d.toString().trim();
     if([1,2,3,4,5,6,7,8,9].includes(input)) {
-      board[Math.floor((input - 1)/3)][(input - 1)%3] = turn;
-      turn = turn === 'X' ? 'O' : 'X';
+      if(board[Math.floor((input - 1)/3)][(input - 1)%3] === ' ') {
+        board[Math.floor((input - 1)/3)][(input - 1)%3] = turn;
+        if(checkVictory()) {
+          victor = turn;
+        }
+        turn = turn === 'X' ? 'O' : 'X';
+      }
     }
 
   board.map((row, i)=>{
     console.log(row.join(' |'))
     if(i < board.length - 1) {console.log('__ __ __')}
   })
-  console.log(`it is player ${turn}'s turn`);
+  if(victor) {
+    console.log(`Congratulations player ${victor} won!`);
+  } else {
+    console.log(`it is player ${turn}'s turn`);
+  }
 });
